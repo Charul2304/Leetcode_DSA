7bf33887eeb968a0faa4387aecf882b2bed4236f
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-06-04 15:55:02 +0530

    Time: 20 ms (72.38%) | Memory: 31.2 MB (58.52%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int f(vector<int>& nums, int goal){
-        int i=0,j=0,sum=0,count=0;
         if(goal<0) return 0;
-        while(j<nums.size()){
-            sum+=nums[j];
+        int left=0;
+        int right=0;
+        int sum=0;
+        int count=0;
+        while(right<nums.size()){
+            sum+=nums[right];
             while(sum>goal){
-                sum-=nums[i];
-                i++;
+                sum=sum-nums[left];
+                left++;
             }
-            count+=(j-i+1);
-            j++;
+            count+=(right-left+1);
+            right++;
+            
         }
         return count;
     }
