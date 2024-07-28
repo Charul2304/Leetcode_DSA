a3b947ae60d38f36236922a32f0e2baf00ed4a19
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-07-28 15:33:56 +0530

    Time: 86 ms (80.67%) | Memory: 61.9 MB (49.59%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,50 +1,22 @@
-// class Solution {
-// public:
-//     int maxOperations(vector<int>& nums, int k) {
-//         sort(nums.begin(),nums.begin());
-//         int n=nums.size();
-//         int i=0;
-//         int j=n-1;
-//         int count=0;
-//         while(i<j){
-//             if(nums[i]+nums[j]==k){
-//                 count++;
-//                 i++;
-//                 j--;
-//             }
-//             else if(nums[i]+nums[j]<k){
-//                 i++;
-//             }
-//             else{
-//                 j--;
-//             }
-//         }
-//         return count;
-//     }
-// };
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
-        int n = nums.size();
-        int count = 0;
-        int left = 0;
-        int right = n-1;
-        while(left < right)
-        {
-            if(nums[left] + nums[right] == k)
-            {
+        int n=nums.size();
+        int i=0;
+        int j=n-1;
+        int count=0;
+        while(i<j){
+            if(nums[i]+nums[j]==k){
                 count++;
-                left++;
-                right--;
+                i++;
+                j--;
             }
-            else if(nums[left] + nums[right] > k)
-            {
-                right--;
+            else if(nums[i]+nums[j]<k){
+                i++;
             }
-            else
-            {
-                left++;
+            else{
+                j--;
             }
         }
         return count;
