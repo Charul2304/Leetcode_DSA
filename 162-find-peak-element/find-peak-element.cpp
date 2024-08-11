a0e05083552cab1e85e00ab70e0f6287332b78b7
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-08-11 22:40:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (6.55%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -8,9 +8,15 @@ public:
         int low=1,high=n-2;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-            else if(nums[mid]>nums[mid-1]) low=mid+1;
-            else high=mid-1;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
         return -1;
     }
