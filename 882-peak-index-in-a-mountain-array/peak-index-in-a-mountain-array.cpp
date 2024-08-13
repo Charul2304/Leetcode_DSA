263e80078991c32bba0b365c6c9daa81bbaf84b0
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-08-13 14:08:13 +0530

    Time: 83 ms (44.10%) | Memory: 62.4 MB (28.81%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -6,11 +6,11 @@ public:
         int high=n-1;
         while(low<=high){
             int mid=high+(low-high)/2;
-            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
-            else if(arr[mid] > arr[mid-1]){
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
+            else if(arr[mid]>arr[mid-1]){
                 low=mid+1;
             }
-            else if(arr[mid] > arr[mid+1]){
+            else if(arr[mid]>arr[mid+1]){
                 high=mid-1;
             }
         }
