4581571488e74dcd2b8f92822833f49feda5a891
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-08-13 12:52:18 +0530

    Time: 422 ms (5.02%) | Memory: 172.1 MB (7.89%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,44 +1,15 @@
-// class Solution {
-// public:
-//     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-//         vector<vector<int>> ans;
-//         priority_queue<pair<int,vector<int>>> pq; 
-//         for(int i=0;i<nums1.size();i++){
-//             for(int j=0;j<nums2.size();j++){
-//                 int sum=nums1[i]+nums2[j];
-//                 if(pq.size()<k){
-//                     pq.push({sum,{nums1[i],nums2[j]}});
-//                 }
-//                 else if(sum<pq.top().first){
-//                     pq.pop();
-//                     pq.push({sum,{nums1[i],nums2[j]}});
-//                 }
-//                 else if(sum>pq.top().first){
-//                     break;
-//                 }
-//             }
-//         }
-//         while(!pq.empty()){
-//             ans.push_back(pq.top().second);
-//             pq.pop();
-//         }
-//         return ans;
-//     }
-// };
+
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<vector<int>>ans;
-        priority_queue<pair<int,pair<int,int>>>pq;     //Max Heap
+        priority_queue<pair<int,vector<int>>> pq;
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
-               int sum=nums1[i]+nums2[j];
-                 //only for first iteration until size is k
-                if(pq.size() < k){
+                int sum=nums1[i]+nums2[j];
+                if(pq.size()<k){
                     pq.push({sum,{nums1[i],nums2[j]}});
                 }
-                 // here compare sum with the top of pq 
-                else if(pq.top().first > sum){
+                else if(pq.top().first>sum){
                     pq.pop();
                     pq.push({sum,{nums1[i],nums2[j]}});
                 }
@@ -47,8 +18,9 @@ public:
                 }
             }
         }
-        while(!pq.empty()){
-            ans.push_back({pq.top().second.first,pq.top().second.second});
+        vector<vector<int>> ans;
+        while(k--){
+            ans.push_back(pq.top().second);
             pq.pop();
         }
         return ans;
