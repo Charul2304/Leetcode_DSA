faad8d6e26b0cd94ec7b73262bc112720635cf0a
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-08-15 14:41:41 +0530

    Time: 3 ms (29.50%) | Memory: 7.7 MB (24.03%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,13 @@
-//space optimisation approach
 class Solution {
 public:
-
+    int f(int n,vector<int>& dp){
+        if(n<0) return 0;
+        if(n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    }
     int climbStairs(int n) {
-        int prev2=1;
-        int prev=1;
-    
-        for(int i=2;i<=n;i++){
-            int curri=prev+prev2;
-            prev2=prev;
-            prev=curri;
-        }
-        return prev;
-       
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
     }
 };
