b446306ac5075de2a8aff5bc98203f2f8069aff8
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-08-18 11:29:19 +0530

    Time: 8 ms (84.58%) | Memory: 27.4 MB (42.47%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,13 +12,13 @@
 class Solution {
 public:
     TreeNode* solve(vector<int>& preorder,int preStart,int preEnd,
-                    vector<int>& inorder,int inStart,int inEnd,map<int,int>& mp){
+    vector<int>& inorder,int inStart,int inEnd,map<int,int>& mp){
         if(preStart>preEnd || inStart>inEnd) return NULL;
         TreeNode* root=new TreeNode(preorder[preStart]);
         int inRoot=mp[root->val];
-        int numsLeft=inRoot-inStart;
-        root->left=solve(preorder,preStart+1,preStart+numsLeft,inorder,inStart,inRoot-1,mp);
-        root->right=solve(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1,inEnd,mp);
+        int numsleft=inRoot-inStart;
+        root->left=solve(preorder,preStart+1,preStart+numsleft,inorder,inStart,inRoot-1,mp);
+        root->right=solve(preorder,preStart+numsleft+1,preEnd,inorder,inRoot+1,inEnd,mp);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
@@ -26,8 +26,6 @@ public:
         for(int i=0;i<inorder.size();i++){
             mp[inorder[i]]=i;
         }
-        TreeNode* root=solve(preorder,0,preorder.size()-1,
-                            inorder,0,inorder.size()-1,mp);
-        return root;
+        return solve(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mp);
     }
 };
