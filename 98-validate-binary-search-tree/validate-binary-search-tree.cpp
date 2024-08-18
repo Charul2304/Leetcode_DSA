358f4764df8befac6593d4217ba35d0c9e7f8475
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-08-18 18:01:53 +0530

    Time: 10 ms (41.52%) | Memory: 20.4 MB (56.50%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -9,16 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    bool check(TreeNode* root,long long int lb,long long int rb){
+    bool check(TreeNode* root,long long int lb,long long int ub){
         if(root==NULL) return true;
-        if(root->val<=lb || root->val >= rb) return false;
-        return check(root->left,lb,root->val) && check(root->right,root->val,rb);
+        if(root->val<=lb || root->val>=ub) return false;
+        return check(root->left,lb,root->val) && check(root->right,root->val,ub);
     }
     bool isValidBST(TreeNode* root) {
-        long long int lb= LLONG_MIN; long long int rb= LLONG_MAX;
-        return check(root,lb,rb);
+        long long int lb=LLONG_MIN;
+        long long int ub=LLONG_MAX;
+        return check(root,lb,ub);
     }
 };
