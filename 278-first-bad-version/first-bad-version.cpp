e2bdf84fb9a9dcaf0719887c46afe08fc0bd8184
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-08-24 19:03:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (10.90%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low=0,high=n;
+        int ans=-1;
+        while(low<=high){
+            int mid=high-(high-low)/2;
+            if(isBadVersion(mid)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
