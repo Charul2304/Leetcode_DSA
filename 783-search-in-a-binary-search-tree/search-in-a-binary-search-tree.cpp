fdd8b1a9854a7ec479479589f8d03502d983910b
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-10-12 22:26:06 +0530

    Time: 31 ms (52.20%) | Memory: 33.5 MB (82.40%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -14,14 +14,14 @@ public:
     TreeNode* searchBST(TreeNode* root, int val) {
         if(root==NULL) return NULL;
         while(root){
-            if(root->val>val){
+            if(root->val==val){
+                return root;
+            }
+            else if(root->val>val){
                 root=root->left;
             }
-            else if(root->val<val){
-                root=root->right;
-            }
             else{
-                return root;
+                root=root->right;
             }
         }
         return NULL;
