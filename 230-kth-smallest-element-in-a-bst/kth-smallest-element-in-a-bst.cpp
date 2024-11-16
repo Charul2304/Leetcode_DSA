1aa0f319424d911e70954f8ab052ef43c6857f78
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-11-16 15:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (97.17%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -9,21 +9,21 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-int ans=-1;
 class Solution {
 public:
-    void solve(TreeNode* root, int& k){
+    void solve(TreeNode* root,int k,int& count,int& ans){
         if(root==NULL) return;
-        solve(root->left,k);
-        k--;
-        if(k==0){
+        solve(root->left,k,count,ans);
+        count++;
+        if(count==k){
             ans=root->val;
             return;
         }
-        solve(root->right,k);
+        solve(root->right,k,count,ans);
     }
     int kthSmallest(TreeNode* root, int k) {
-        solve(root,k);
+        int count=0,ans=0;
+        solve(root,k,count,ans);
         return ans;
     }
 };
