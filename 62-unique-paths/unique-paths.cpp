32e169462f494b2182e833586760f9ebb1c788f3
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-11-21 15:19:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (20.86%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int f(int i,int j,vector<vector<int>>& dp){
-        if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
+        if(i==0 && j==0) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
         int up=f(i-1,j,dp);
         int left=f(i,j-1,dp);
