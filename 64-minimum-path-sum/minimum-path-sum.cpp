b3b0874e2684dfea2af9960409fc5df5e775c85c
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-11-21 15:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (28.55%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int f(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
-        if(i==0 && j==0) return grid[0][0];
         if(i<0 || j<0) return 1e9;
+        if(i==0 && j==0) return grid[0][0];
         if(dp[i][j]!=-1) return dp[i][j];
         int up=grid[i][j]+f(i-1,j,grid,dp);
         int left=grid[i][j]+f(i,j-1,grid,dp);
