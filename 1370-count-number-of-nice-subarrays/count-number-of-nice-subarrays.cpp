6f14e59ce8055e7111755fd2b69d9ce0c0ea1d5d
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-12-24 19:37:33 +0530

    Time: 15 ms (32.93%) | Memory: 71.5 MB (42.08%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int goal){
-        if(goal<0) return 0;
-        int n=nums.size();
-        int i=0,j=0;
+    int f(vector<int>& nums, int k){
         int count=0;
         int sum=0;
+        int i=0,j=0;
+        int n=nums.size();
         while(j<n){
             sum+=(nums[j]%2);
-            while(sum>goal){
+            while(sum>k){
                 sum-=(nums[i]%2);
                 i++;
             }
-            
             count+=(j-i+1);
             j++;
         }
