8476c41e96d131b96e6873a935b6e4536e3d24de
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-12-24 16:34:25 +0530

    Time: 16 ms (11.77%) | Memory: 19.6 MB (47.35%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         vector<int> adj[n];
+        vector<int> indegree(n,0);
         for(auto x:prerequisites){
             adj[x[0]].push_back(x[1]);
         }
-        vector<int> indegree(n,0);
         for(int i=0;i<n;i++){
             for(auto it:adj[i]){
                 indegree[it]++;
@@ -13,20 +13,18 @@ public:
         }
         queue<int> q;
         for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
+            if(indegree[i]==0) q.push(i);
         }
-        int topocount=0;
+        int count=0;
         while(!q.empty()){
             int node=q.front();
             q.pop();
-            topocount++;
+            count++;
             for(auto it:adj[node]){
                 indegree[it]--;
                 if(indegree[it]==0) q.push(it);
             }
         }
-        return topocount==n;
+        return count==n;
     }
 };
