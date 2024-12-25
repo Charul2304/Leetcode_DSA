c4c6027b8904feb8c1065500c953138c7351fa37
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2024-12-25 12:02:32 +0530

    Time: 21 ms (54.71%) | Memory: 38.7 MB (29.81%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,9 +3,9 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> vis(m,vector<int>(n,0));
         vector<vector<int>> dist(m,vector<int>(n,0));
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
@@ -18,16 +18,15 @@ public:
         int dc[4]={0,1,0,-1};
         while(!q.empty()){
             auto p=q.front();
+            q.pop();
             int row=p.first.first;
             int col=p.first.second;
             int dis=p.second;
             dist[row][col]=dis;
-            q.pop();
             for(int i=0;i<4;i++){
                 int nrow=row+dr[i];
                 int ncol=col+dc[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n &&
-                mat[nrow][ncol] && !vis[nrow][ncol]){
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && mat[nrow][ncol] && !vis[nrow][ncol]){
                     q.push({{nrow,ncol},dis+1});
                     vis[nrow][ncol]=1;
                 }
