1e8162cd2f9658d9ae3e7d32a653d9d4d91657fa
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2025-06-26 15:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (41.98%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,14 +4,20 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         int low=0;
-        int high=n*m-1;
+        int high=m*n-1;
         while(low<=high){
             int mid=(low+high)/2;
             int row=mid/n;
             int col=mid%n;
-            if(matrix[row][col]==target) return true;
-            else if(matrix[row][col]<target) low=mid+1;
-            else high=mid-1;
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
         return false;
     }
