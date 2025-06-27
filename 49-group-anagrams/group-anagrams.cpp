c667ea3c36ec18ac72afda22855a1c33c4475822
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2025-06-27 19:21:16 +0530

    Time: 23 ms (34.11%) | Memory: 26 MB (48.62%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
         unordered_map<string,vector<string>> mp;
         for(auto word:strs){
             string key=word;
             sort(key.begin(),key.end());
-            // if(mp.find(key)==mp.end()){
-            //     mp[key]={word};
-            // }
-            // else{
-            //     mp[key].push_back(word);
-            // }
             mp[key].push_back(word);
         }
-        vector<vector<string>> ans;
         for(auto x:mp){
             ans.push_back(x.second);
         }
