179b6c05cf730e6aba007395787e3b70dd294599
Charul_Nampalliwar <125807803+Charul2304@users.noreply.github.com>
2025-09-21 14:31:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (45.99%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<n-1;i++){
             if(nums[i]>nums[i+1]) count++;
         }
-        if(nums[n-1]>nums[0]) count++;
+        if(nums[0]<nums[n-1]) count++;
         return count<=1;
     }
 };
